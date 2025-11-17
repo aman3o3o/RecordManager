1842edd119ba1f5d56df1e28ac9105ab33513b38
Aman Prasad <amanpd14150518@gmail.com>
2025-11-17 17:23:44 +0530

    updated code

---
+++ frontend/src/Component/RegisteredUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RegisteredUser = () => {
+  return (
+    <>
+    <div>
+        <h1>List of registered users</h1>
+        
+    </div>
+    </>
+  )
+}
+
+export default RegisteredUser
